bdabcb70ec75e0134365d056e7e2812274ea72b4
branchprotection <branchprotection>
2021-02-24 01:52:09 -0600

    Revert "test"
    This reverts commit 222fae0f9d7b48a03c5cf8476fce71be256841dd.

--- .github/workflows/owner-check.js
+++ .github/workflows/owner-check.js
@@ -4,6 +4,7 @@ const owners = JSON.parse(fs.readFileSync("../../codeowners.json"));
 const PASS = "PASS";
 const FAIL = "FAIL";
 
+
 const user = process.argv[2];
 const branch = process.argv[3].split("refs/heads/")[1];
 
