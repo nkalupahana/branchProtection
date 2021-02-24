8571284e6af310b560dce53e3261927878b4dfc0
nkalupahana <avishkank@gmail.com>
2021-02-24 11:13:18 -0600

    Switch to pulling codeowners from link

--- .github/workflows/owner-check.js
+++ .github/workflows/owner-check.js
@@ -2,38 +2,28 @@ const fs = require("fs");
 const https = require("https");
 
 (async () => {
-    const owners = await new Promise((res, _rej) => {
+    const owners = await new Promise((resolve, _) => {
         https.get("https://cdn.jsdelivr.net/gh/nkalupahana/branchProtection@main/codeowners.json", res => {
             res.on("data", data => {
-                res(JSON.stringify(data));
-            })
+                resolve(JSON.parse(data.toString()));
+            });
         });
     });
-    
-    console.log(owners);
+
+    const user = process.argv[2];
+    const branch = process.argv[3].split("refs/heads/")[1];
+
+    if (!owners[branch] || owners[branch].includes(user)) {
+        console.log("PASS");
+        return;
+    }
+
+    console.log("REJECT");
+    const { exec } = require('child_process');
+    exec("chmod a+x revert-commit.bash; ./revert-commit.bash", (err, stdout, stderr) => {
+        console.log(err);
+        console.log(stdout);
+        console.log(stderr);
+    });
+
 })();
-
-
-
-/*
-
-const PASS = "PASS";
-const FAIL = "FAIL";
-
-
-const user = process.argv[2];
-const branch = process.argv[3].split("refs/heads/")[1];
-
-if (!owners[branch] || owners[branch].includes(user)) {
-    console.log(PASS);
-    return;
-}
-
-const { exec } = require('child_process');
-exec("chmod a+x revert-commit.bash; ./revert-commit.bash", (err, stdout, stderr) => {
-    console.log(err);
-    console.log(stdout);
-    console.log(stderr);
-});
-
-*/
