3ad6b126da8779ee7e5840b7770903edf62e574e
Nisala Kalupahana <avishkank@gmail.com>
2021-02-24 01:56:29 -0600

    Update revert-commit.bash

--- .github/workflows/revert-commit.bash
+++ .github/workflows/revert-commit.bash
@@ -4,6 +4,6 @@ echo REJECT
 git stash
 git config --global user.email "branchprotection"
 git config --global user.name "branchprotection"
-#git revert HEAD --no-edit
+git revert HEAD --no-edit
 git status
-#git push
+git push
